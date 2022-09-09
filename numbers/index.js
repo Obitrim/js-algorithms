7d4ed310abdcc0af41a379b326506662d379bd1e
Obitrim <pevangelist516@gmail.com>
2022-09-09 09:31:22 +0000

    feature: test cases

--- numbers/index.js
+++ numbers/index.js
@@ -1,7 +1,21 @@
-const isPrime = require("./primality-test");
+// const isPrime = require("./primality-test");
+const { findLCM } = require("./LCM");
+// const primeFactors = require("./prime-factors");
 
-console.log(isPrime(59) ? "yes" : "no");
-console.log(isPrime(40) ? "yes" : "no");
-console.log(isPrime(32) ? "yes" : "no");
-console.log(isPrime(17) ? "yes" : "no");
-console.log(isPrime(23) ? "yes" : "no");
+// ------------------------PRIMALITY CHECK--------------------------
+
+// console.log(isPrime(59) ? "yes" : "no");
+// console.log(isPrime(40) ? "yes" : "no");
+// console.log(isPrime(32) ? "yes" : "no");
+// console.log(isPrime(17) ? "yes" : "no");
+// console.log(isPrime(23) ? "yes" : "no");
+
+// ------------------------PRIME FACTORS--------------------------
+
+// console.log(primeFactors(54));
+// console.log(primeFactors("54"));
+// console.log(primeFactors("54"));
+
+// ------------------------LCM & GCM------------------------------
+
+// console.log("lcm", findLCM(3, 4));
